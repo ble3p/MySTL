e0ee5873efc4cac2d68ac0f9450185adf587368e
ble3_p <ble3_p@163.com>
2023-07-25 19:58:55 +0800

    添加algobase算法

--- algobase.h
+++ algobase.h
@@ -128,19 +128,93 @@ copy(InputIter first, InputIter last, OutputIter result)
   return unchecked_copy(first, last, result);
 }
 
+/*****************************************************************************************/
+// copy_backward
+// 将 [first , last) 区间内的元素拷贝到 [result - (last - first) , result)内
+/*****************************************************************************************/
+// unchecked_copy_backward_cat 的 bidirectional_iterator_tag 版本
+template <class BidrectionalIter1, class BidrectionalIter2>
+BidrectionalIter2
+unchecked_copy_backward_cat(BidrectionalIter1 first, BidrectionalIter1 last,
+                            BidrectionalIter2 result, MyStl::bidirectional_iterator_tag)
+{
+  while (first != last)
+    *--result = *--last;
+  return result;
+}
+
+
+// unchecked_copy_backward_cat 的 random_access_iterator_tag 版本
+template <class RandomIter1, class BidrectionalIter2>
+BidrectionalIter2
+unchecked_copy_backward_cat(RandomIter1 first, RandomIter1 last,
+                            BidrectionalIter2 result, MyStl::random_access_iterator_tag)
+{
+  for(auto n = last - first; n > 0; --n)
+    *--result = *--last;
+  return result;
+}
+
+template <class BidrectionalIter1, class BidrectionalIter2>
+BidrectionalIter2
+unchecked_copy_backward(BidrectionalIter1 first, BidrectionalIter1 last,
+                        BidrectionalIter2 result)
+{
+  return unchecked_copy_backward_cat(first, last ,result, iterator_category(first));
+}
+
+// 为 trivially_copy_assignable 类型提供特化版本
+template <class Tp, class Up>
+typename std::enable_if<
+  std::is_same<typename std::remove_reference<Tp>::type, Up>::value &&
+  std::is_trivially_copy_assignable<Up>::value,
+  Up*>::type
+unchecked_copy_backward(Tp * first, Tp* last, Up* result)
+{
+  const auto n = static_cast<size_t>(last - first);
+  if (n != 0)
+  {
+    result -= n;
+    std::memmove(result, first, sizeof(Up) * n);
+  }
+  return result;
+}
+
+template <class BidrectionalIter1, class BidrectionalIter2>
+BidrectionalIter2
+copy_backward(BidrectionalIter1 first , BidrectionalIter1 last , BidrectionalIter2 result)
+{
+  return unchecked_copy_backward(first, last ,result);
+}
+
 
 /*****************************************************************************************/
-// min
-// 取两者中的较小值，语义相等时保证返回第一个参数
+// copy_if
+// 把[first, last) 内满足医院操作 unary_pre 的元素拷贝到以result为起始的位置上
 /*****************************************************************************************/
+template <class InputIter, class OutputIter, class UnaryPredicate>
+OutputIter
+copy_if(InputIter first, InputIter last, OutputIter result, UnaryPredicate unary_pred)
+{
+  for (; first != last; ++first)
+  {
+    if (unary_pred(*first))
+      *result++ = *first;
+  }
+  return result;
+}
+
 /*****************************************************************************************/
-// min
-// 取两者中的较小值，语义相等时保证返回第一个参数
+// copy_n
+// 把[first, first + n)区间上的元素拷贝到 [result, result + n) 上\
+// 返回一个pair分别指向拷贝结束的尾部
 /*****************************************************************************************/
-/*****************************************************************************************/
-// min
-// 取两者中的较小值，语义相等时保证返回第一个参数
-/*****************************************************************************************/
+// template <class InputIter, class OutputIter, class Size>
+// MyStl::pair<InputIter, OutputIter>
+// unchecked_copy_n(InputIter first, Size n, OutputIter result, MyStl::input_iterator_tag)
+// {
+
+// }
 /*****************************************************************************************/
 // min
 // 取两者中的较小值，语义相等时保证返回第一个参数
