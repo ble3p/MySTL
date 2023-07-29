2139eb8e1658b03c815ba160bc849aa238ba0dd1
ble3_p <ble3_p@163.com>
2023-07-29 16:29:24 +0800

    添加计算指针间距离和移动指针的算法

--- iterator.h
+++ iterator.h
@@ -156,9 +156,70 @@ value_type(const Iterator&)
   return static_cast<typename iterator_traits<Iterator>::value_type*>(0);
 }
 
+// 以下函数用于计算迭代器间的距离
 
+// distance 的 input_iterator_tag 的版本
+template <class InputIterator>
+typename iterator_traits<InputIterator>::difference_type
+distance_dispatch(InputIterator first, InputIterator last, input_iterator_tag)
+{
+    typename iterator_traits<InputIterator>::difference_type n = 0;
+    while (first != last)
+    {
+        ++first;
+        ++n;
+    }
+    return n;
+}
 
+// distance 的random_access_iterator_tag 的版本
+template <class RandomIter>
+typename iterator_traits<RandomIter>::difference_type
+distance_dispatch(RandomIter first, RandomIter last,
+                  random_access_iterator_tag)
+{
+    return last - first;
+}
 
+template <class InputIterator>
+typename iterator_traits<InputIterator>::difference_type
+distance(InputIterator first, InputIterator last)
+{
+    return distance_dispatch(first, last, iterator_category(first));
+}
+
+// 以下函数用于让迭代器前进n个距离
+
+// advance 的 input_iterator_tag 的版本
+template <class InputIterator, class Distance>
+void advance_dispatch(InputIterator &i, Distance n, input_iterator_tag)
+{
+    while (n--)
+        ++i;
+}
+
+// advance 的 bidirectional_iterator_tag的版本
+template <class BidrectionalIterator, class Distance>
+void advance_dispatch(BidrectionalIterator &i, Distance n, bidirectional_iterator_tag)
+{
+    if (n >= 0)
+        while (n--) ++i;
+    else
+        while (n++) --i;
+}
+
+// advance 的 random_access_iterator_tag 的版本
+template <class BidrectionalIterator, class Distance>
+void advance_dispatch(BidrectionalIterator &i, Distance n, random_access_iterator_tag)
+{
+    i += n;
+}
+
+template <class InputIterator, class Distance>
+void advance(InputIterator &i, Distance n)
+{
+    advance_dispatch(i, n, iterator_category(i));
+}
 }
 
 #endif
