df5d3cd0edeeedf716819b8fb46b6b2700e1f5b7
ble3_p <ble3_p@163.com>
2023-08-01 22:45:46 +0800

    添加reverse_iterator指针类和相应算法

--- algo.h
+++ algo.h
@@ -180,6 +180,8 @@ template <class ForwardIter, class T>
 ForwardIter
 search_n(ForwardIter first, ForwardIter last, size_t n, const T &value)
 {
+    if (n <= 0)
+        return first;
     auto d = MyStl::distance(first, last);
     if (d < n)
         return last;
@@ -211,6 +213,8 @@ template <class ForwardIter, class T, class Compare>
 ForwardIter
 search_n(ForwardIter first, ForwardIter last, size_t n, const T &value, Compare comp)
 {
+    if (n <= 0)
+        return first;
     auto d = MyStl::distance(first, last);
     if (d < n)
         return last;
@@ -239,11 +243,142 @@ search_n(ForwardIter first, ForwardIter last, size_t n, const T &value, Compare 
 }
 
 
+/*****************************************************************************************/
+// find_end
+// 在[first1, last1)区间中查找[first2, last2)最后一次出现的地方，若不存在返回 last1
+/*****************************************************************************************/
+// find_end_dispatch 的 forward_iterator_tag 版本
+template <class ForwardIter1, class ForwardIter2>
+ForwardIter1
+find_end_dispatch(ForwardIter1 first1, ForwardIter1 last1,
+                  ForwardIter2 first2, ForwardIter2 last2,
+                  forward_iterator_tag, forward_iterator_tag)
+{
+    if (first2 == last2)
+    {
+        return last1;
+    }
+    else
+    {
+        auto result = last1;
+        while(true)
+        {
+            auto new_result = MyStl::search(first1, last1, first2, last2);
+            if (new_result == last1)
+            {
+                return result;
+            }
+            else
+            {
+                result = new_result;
+                first1 = new_result;
+                ++first1;
+            }
+        }
+    }
+
+}
+
+template <class BidrectionalIter1, class BidrectionalIter2>
+BidrectionalIter1
+find_end_dispatch(BidrectionalIter1 first1, BidrectionalIter1 last1,
+                  BidrectionalIter2 first2, BidrectionalIter2 last2,
+                  bidirectional_iterator_tag, bidirectional_iterator_tag)
+{
+    typedef reverse_iterator<BidrectionalIter1> rev_iter1;
+    typedef reverse_iterator<BidrectionalIter2> rev_iter2;
+    rev_iter1 rlast1(first1); // 构造反向迭代器对象
+    rev_iter2 rlast2(first2);
+    rev_iter1 rresult = MyStl::search(rev_iter1(last1), rlast1, rev_iter2(last2), rlast2);
+    if (rresult == rlast1)
+    {
+        return last1;
+    }
+    else 
+    {
+        auto result = rresult.base();
+        MyStl::advance(result, MyStl::distance(first2, last2));
+        return result; // 逆序查找，然后进行指向看透
+    }
+
+}
+
+template <class ForwardIter1, class ForwardIter2>
+ForwardIter1
+find_end(ForwardIter1 first1, ForwardIter2 last1,
+         ForwardIter2 first2, ForwardIter2 last2)
+{
+    typedef typename iterator_traits<ForwardIter1>::iterator_category Category1;
+    typedef typename iterator_traits<ForwardIter2>::iterator_category Category2;
+    return MyStl::find_end_dispatch(first1, last1, first2, last2, Category1(), Category2());
+}
 
 
+template <class ForwardIter1, class ForwardIter2, class Compare>
+ForwardIter1
+find_end_dispatch(ForwardIter1 first1, ForwardIter1 last1,
+                  ForwardIter2 first2, ForwardIter2 last2,
+                  forward_iterator_tag, forward_iterator_tag, Compare comp)
+{
+    if (first2 == last2)
+    {
+        return last1;
+    }
+    else
+    {
+        auto result = last1;
+        while(true)
+        {
+            auto new_result = MyStl::search(first1, last1, first2, last2, comp);
+            if (new_result == last1)
+            {
+                return result;
+            }
+            else
+            {
+                result = new_result;
+                first1 = new_result;
+                ++first1;
+            }
+        }
+    }
+
+}
+
+template <class BidrectionalIter1, class BidrectionalIter2, class Compare>
+BidrectionalIter1
+find_end_dispatch(BidrectionalIter1 first1, BidrectionalIter1 last1,
+                  BidrectionalIter2 first2, BidrectionalIter2 last2,
+                  bidirectional_iterator_tag, bidirectional_iterator_tag, Compare comp)
+{
+    typedef reverse_iterator<BidrectionalIter1> rev_iter1;
+    typedef reverse_iterator<BidrectionalIter2> rev_iter2;
+    rev_iter1 rlast1(first1); // 构造反向迭代器对象
+    rev_iter2 rlast2(first2);
+    rev_iter1 rresult = MyStl::search(rev_iter1(last1), rlast1, rev_iter2(last2), rlast2, comp);
+    if (rresult == rlast1)
+    {
+        return last1;
+    }
+    else 
+    {
+        auto result = rresult.base();
+        MyStl::advance(result, MyStl::distance(first2, last2));
+        return result; // 逆序查找，然后进行指向看透
+    }
+
+}
 
 
-
+template <class ForwardIter1, class ForwardIter2, class Compare>
+ForwardIter1
+find_end(ForwardIter1 first1, ForwardIter2 last1,
+         ForwardIter2 first2, ForwardIter2 last2, Compare comp)
+{
+    typedef typename iterator_traits<ForwardIter1>::iterator_category Category1;
+    typedef typename iterator_traits<ForwardIter2>::iterator_category Category2;
+    return MyStl::find_end_dispatch(first1, last1, first2, last2, Category1(), Category2(), comp);
+}
 
 
 
--- iterator.h
+++ iterator.h
@@ -220,7 +220,140 @@ void advance(InputIterator &i, Distance n)
 {
     advance_dispatch(i, n, iterator_category(i));
 }
+
+
+/*****************************************************************************************/
+
+// 模板类 : reverse_iterator
+// 代表反向迭代器，使前进为后退，后退为前进
+template <class Iterator>
+class reverse_iterator
+{
+private:
+    Iterator current;
+public:
+    typedef typename iterator_traits<Iterator>::iterator_category iterator_category;
+    typedef typename iterator_traits<Iterator>::value_type        value_type;
+    typedef typename iterator_traits<Iterator>::difference_type   difference_type;
+    typedef typename iterator_traits<Iterator>::pointer           pointer;
+    typedef typename iterator_traits<Iterator>::reference         reference;
+
+    typedef Iterator                                              iterator_type;
+    typedef reverse_iterator<Iterator>                            self;
+
+public:
+    reverse_iterator() = default;
+    explicit reverse_iterator(iterator_type i) : current(i) {}
+    reverse_iterator(const self& rhs) : current(rhs.current) {}
+
+public:
+    iterator_type base() const
+    {
+        return current;
+    }
+
+    // 对应正向迭代器的前一个位置,为了让rbegin和 end对齐，rend和begin对齐，所以正向迭代器指向为反向迭代器指向的后一个，要向前一位
+    reference operator* () const
+    {
+        auto temp = current;
+        return *--temp;
+    }
+    pointer operator->() const
+    {
+        return &(operator*());
+    }
+
+    self& operator++()
+    {
+        --current;
+        return *this;
+    }
+    self operator++(int)
+    {
+        self tmp = *this;
+        --current;
+        return tmp;
+    }
+    self& operator--()
+    {
+        ++current;
+        return *this;
+    }
+
+    self operator--(int)
+    {
+        self tmp = *this;
+        ++current;
+        return tmp;
+    }
+
+    self& operator+=(difference_type n)
+    {
+        current -= n;
+        return *this;
+    }
+    
+    self operator+(difference_type n) const
+    {
+        return self(current - n); // 构造函数
+    }
+
+    // 返回的是第n个位置的指向的值的引用
+    reference operator[](difference_type n) const
+    {
+        return *(*this + n);
+    }
+};
+
+// 重载 operator-
+template <class Iterator>
+typename reverse_iterator<Iterator>::difference_type
+operator-(const reverse_iterator<Iterator>& lhs,
+          const reverse_iterator<Iterator>& rhs)
+{
+    return rhs.base() - lhs.base();
 }
+
+template<class Iterator>
+bool operator==(const reverse_iterator<Iterator>& lhs,
+                const reverse_iterator<Iterator>& rhs)
+{
+    return lhs.base() == rhs.base();
+}
+
+template<class Iterator>
+bool operator<(const reverse_iterator<Iterator>& lhs,
+                const reverse_iterator<Iterator>& rhs)
+{
+    return lhs.base() < rhs.base();
+}
+template<class Iterator>
+bool operator!=(const reverse_iterator<Iterator>& lhs,
+                const reverse_iterator<Iterator>& rhs)
+{
+    return !(lhs == rhs);
+}
+template<class Iterator>
+bool operator>(const reverse_iterator<Iterator>& lhs,
+                const reverse_iterator<Iterator>& rhs)
+{
+    return lhs.base() > rhs.base();
+}
+template<class Iterator>
+bool operator<=(const reverse_iterator<Iterator>& lhs,
+                const reverse_iterator<Iterator>& rhs)
+{
+    return !(lhs > rhs);
+}
+template<class Iterator>
+bool operator>=(const reverse_iterator<Iterator>& lhs,
+                const reverse_iterator<Iterator>& rhs)
+{
+    return !(lhs < rhs);
+
+} 
+
+} // MyStl
 
 #endif
 
