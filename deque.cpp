8b609a34f3622985224939896ec484a9b77d3376
ble3_p <ble3_p@163.com>
2023-08-22 14:07:07 +0800

    add deque

---
+++ deque.cpp
@@ -0,0 +1,59 @@
+#ifndef _MYSTL_DEQUE_H_
+#define _MYSTL_DEQUE_H_
+
+
+namespace MyStl
+{
+
+template <class T, class allocator, class buf_size>
+class deque
+{
+public:
+    typedef T           value_type;
+    typedef T*          pointer;
+    typedef T&          reference;
+
+
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+} // end namespace 
+
+
+#endif
+
